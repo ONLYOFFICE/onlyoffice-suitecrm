9b83266dfc8cc1de8450d132b79bc4ea0d76a7ab
Antipkin-A <Artem.Antipkin@onlyoffice.com>
2023-03-17 18:27:52 +0300

    format

--- custom/Extension/modules/Administration/Ext/Administration/onlyoffice.php
+++ custom/Extension/modules/Administration/Ext/Administration/onlyoffice.php
@@ -4,37 +4,19 @@
  * http://www.onlyoffice.com
  */
 
-    $admin_option_defs = array();
-    $admin_option_defs['Administration']['ONLYOFFICE'] = array(
-        //Icon name. Available icons are located in ./themes/default/images
-        'Onlyoffice',
+$admin_option_defs = array();
+$admin_option_defs['Administration']['ONLYOFFICE'] = array(
+    'Onlyoffice',
+    'ONLYOFFICE_LINK_NAME',
+    'ONLYOFFICE_LINK_DESCRIPTION',
+    './index.php?module=Onlyoffice&action=settings',
+    'system-settings',
+);
 
-        //Link name label 
-        'ONLYOFFICE_LINK_NAME',
-
-        //Link description label
-        'ONLYOFFICE_LINK_DESCRIPTION',
-
-        //Link URL - For Sidecar modules
-        './index.php?module=Onlyoffice&action=settings',
-
-        //Alternatively, if you are linking to BWC modules
-        'system-settings',
-    );
-
-    $admin_group_header[] = array(
-        //Section header label
-        'ONLYOFFICE_SECTION_HEADER',
-
-        //$other_text parameter for get_form_header()
-        '',
-
-        //$show_help parameter for get_form_header()
-        false,
-
-        //Section links
-        $admin_option_defs, 
-
-        //Section description label
-        'ONLYOFFICE_SECTION_DESCRIPTION'
-    );
+$admin_group_header[] = array(
+    'ONLYOFFICE_SECTION_HEADER',
+    '',
+    false,
+    $admin_option_defs,
+    'ONLYOFFICE_SECTION_DESCRIPTION'
+);
--- custom/Extension/modules/Administration/Ext/Language/en_us.onlyoffice.php
+++ custom/Extension/modules/Administration/Ext/Language/en_us.onlyoffice.php
@@ -4,7 +4,7 @@
  * http://www.onlyoffice.com
  */
 
-    $mod_strings['ONLYOFFICE_LINK_NAME'] = 'ONLYOFFICE Link';
-    $mod_strings['ONLYOFFICE_LINK_DESCRIPTION'] = 'ONLYOFFICE Link Description';
-    $mod_strings['ONLYOFFICE_SECTION_HEADER'] = 'ONLYOFFICE Panel';
-    $mod_strings['ONLYOFFICE_SECTION_DESCRIPTION'] = 'ONLYOFFICE Description';
+$mod_strings['ONLYOFFICE_LINK_NAME'] = 'ONLYOFFICE Link';
+$mod_strings['ONLYOFFICE_LINK_DESCRIPTION'] = 'ONLYOFFICE Link Description';
+$mod_strings['ONLYOFFICE_SECTION_HEADER'] = 'ONLYOFFICE Panel';
+$mod_strings['ONLYOFFICE_SECTION_DESCRIPTION'] = 'ONLYOFFICE Description';
